a455d1c8972886cf48d73dcc717545ac645cf4ae
Mxfalconmx1 <mariofalcon030901@gmail.com>
2026-05-27 00:04:39 -0600

    Create ai_engine.py

---
+++ ai_engine.py
@@ -0,0 +1,5 @@
+# modules/ai_engine.py
+def analizar_con_ia(archivo_pcap):
+    print(f"🧠 Analizando {archivo_pcap} con la neurona...")
+    print("🔮 Predicción: Tráfico 78% benigno, 22% potencialmente malicioso.")
+    print("🛡️ Recomendación: Revisar puertos 4444 y 8080.")
